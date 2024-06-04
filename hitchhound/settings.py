d358a8dd365d9daccae22ee3ea568d9e1a5581ad
Evita Orrock <evita.orrock@gmail.com>
2024-06-04 15:37:29 +0000

    Tweak allowed hosts

--- hitchhound/settings.py
+++ hitchhound/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4uxlu@c(b@^3ls37-_jxnmv&(flidccg*z)=6xiwh0kcq45ds#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-evitaknits-hitchhound-69v294c8trk.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-evitaknits-hitchhound-69v294c8trk.ws.codeinstitute-ide.net', '.herokuapp.com',]
 
 
 # Application definition
