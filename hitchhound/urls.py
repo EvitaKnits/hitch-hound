1b29460574d9a70b4a8ca227f46b13c5f47694ce
Evita Orrock <evita.orrock@gmail.com>
2024-06-04 15:22:07 +0000

    Create hitchound project and first app - issues

---
+++ hitchhound/urls.py
@@ -0,0 +1,24 @@
+"""
+URL configuration for hitchhound project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from issues import views as issues_views
+
+urlpatterns = [
+    path('issues/', issues_views.my_issues, name='issues'),
+    path('admin/', admin.site.urls),
+]
