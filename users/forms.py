65b6ad771396b4d1625f4f7e9b12fb0411045376
Evita Orrock <evita.orrock@gmail.com>
2024-06-12 14:51:44 +0000

    Update base.html and create login form Added another block in the base.html so I can change the navbar per page as needed. So base_signup.html was deleted as it wasn't needed anymore. All html files were updated with this new block. I also created the login form and styled it.

--- users/forms.py
+++ users/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from users.models import User
 
 class CustomUserCreationForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=True)
+    first_name = forms.CharField(max_length=30, required=True,  widget=forms.TextInput(attrs={'autofocus': 'autofocus'}))
     last_name = forms.CharField(max_length=30, required=True)
     email = forms.EmailField(required=True)
     role = forms.ChoiceField(choices=User.ROLE_CHOICES, required=True)
@@ -11,3 +11,4 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'role', 'password1', 'password2')
+
--- users/views.py
+++ users/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from .forms import CustomUserCreationForm
+from django.contrib import messages
+from django.http import HttpResponse
 
 # Create your views here.
 
 def user_login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
 
 def user_logout(request):
     return render(request, 'logout.html', {'active_page': 'logout'})
