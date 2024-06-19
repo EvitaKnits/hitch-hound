e056a2d97a4b167a4338c2d831159b215601baf4
Evita Orrock <evita.orrock@gmail.com>
2024-06-19 14:12:22 +0000

    Fix how models appear in the admin panel

--- issues/admin.py
+++ issues/admin.py
@@ -2,21 +2,27 @@ from django.contrib import admin
 from .models import Issue, UserIssue, Comment
 
 class IssueAdmin(admin.ModelAdmin):
-    readonly_fields = [
-        'issue_id',
-        'reporter'
-    ]
+    list_display = ('title', 'severity', 'type', 'status', 'reporter', 'project_title', 'developer', 'quality_assurance', 'product_manager')
+    fields = ('title', 'description', 'severity', 'type', 'status', 'reporter', 'project_title', 'developer', 'quality_assurance', 'product_manager')
+    search_fields = ('title', 'reporter__username', 'project_title__name')
+    list_filter = ('severity', 'type', 'status', 'project_title')
+
+    def save_model(self, request, obj, form, change):
+        if not obj.reporter:
+            obj.reporter = request.user
+        super().save_model(request, obj, form, change)
 
 class CommentAdmin(admin.ModelAdmin):
-    readonly_fields = [
-    'comment_id',
-    'user_id',
-    'issue_id', 
-    'commented_at',
-    ]
+    list_display = ('comment_text', 'user_id', 'issue_id', 'commented_at')
+    fields = ('comment_text', 'user_id', 'issue_id')
+    readonly_fields = ('comment_id', 'commented_at')
+
+    def save_model(self, request, obj, form, change):
+        if not obj.user_id:
+            obj.user_id = request.user
+        super().save_model(request, obj, form, change)
 
 # Register your models here.
-
 admin.site.register(Issue, IssueAdmin)
 admin.site.register(UserIssue)
 admin.site.register(Comment, CommentAdmin)
--- notifications/admin.py
+++ notifications/admin.py
@@ -2,16 +2,14 @@ from django.contrib import admin
 from .models import Change
 
 class ChangeAdmin(admin.ModelAdmin):
-    readonly_fields = [
-        'change_id',
-        'issue',
-        'user',
-        'changed_at',
-        'field_changed',
-        'old_value',
-        'new_value'
-    ]
+    list_display = ('issue', 'user', 'changed_at', 'field_changed', 'old_value', 'new_value')
+    readonly_fields = ('change_id', 'changed_at')
+    fields = ('issue', 'user', 'field_changed', 'old_value', 'new_value')
+    
+    def save_model(self, request, obj, form, change):
+        if not obj.user:
+            obj.user = request.user
+        super().save_model(request, obj, form, change)
 
 # Register your models here.
-
 admin.site.register(Change, ChangeAdmin)
