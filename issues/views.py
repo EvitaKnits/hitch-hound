60b843a8c3b0d5b4014af44a7288042428793d23
Evita Orrock <evita.orrock@gmail.com>
2024-06-20 10:26:16 +0000

    Make issues table responsive for mobile screens Hid some columns on small/mobile screens. Truncated the title to 50 characters on all devices.

--- issues/views.py
+++ issues/views.py
@@ -2,15 +2,29 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from issues.forms import IssueForm
+from issues.models import Issue
 
 # Create your views here.
 
+# @login_required - uncomment this at the end if I want to restrict to logged in users only. 
 def list_issues(request):
     show_toast = request.session.pop('registration_success', False)
+    sort = request.GET.get('sort', 'title')
+    project_id = request.GET.get('project_id')
+
+    if project_id:
+        project = get_object_or_404(Project, id=project_id)
+        issues = Issue.objects.filter(project_title=project).order_by(sort)
+    else:
+        issues = Issue.objects.all().order_by(sort)
+
     context = {
         'active_page': 'issues',
-        'show_toast': show_toast
+        'show_toast': show_toast,
+        'issues': issues,
+        'project': project if project_id else None,
     }
+    
     return render(request, 'issues.html', context)
 
 # @login_required - uncomment this at the end if I want to restrict to logged in users only. 
