a88621186a345c390e27bd2d77b5671d513ad12d
Evita Orrock <evita.orrock@gmail.com>
2024-06-20 15:55:14 +0000

    Rename some url patterns

--- projects/views.py
+++ projects/views.py
@@ -6,8 +6,8 @@ from django.http import HttpResponse
 def list_projects(request):
     return render(request, 'projects.html', {'active_page': 'projects'})
 
-def new_project(request):
-    return render(request, 'newproject.html', {'active_page': 'projects'})
+def create_project(request):
+    return render(request, 'create_project.html', {'active_page': 'projects'})
 
 def edit_project(request):
-    return render(request, 'editproject.html', {'active_page': 'projects'})
+    return render(request, 'edit_project.html', {'active_page': 'projects'})
