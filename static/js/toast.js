1af08fde4ad32cf7c6f14e9dfa24e2dad5b50227
Evita Orrock <evita.orrock@gmail.com>
2024-06-21 10:59:10 +0000

    Add modal for adding a new issue or project Modal added only to issue listing page as a first step. Scripts were also moved into their own .js files for separation of concerns wherever possible.

--- static/js/toast.js
+++ static/js/toast.js
@@ -2,3 +2,11 @@ $(document).ready(function(){
     $('#registrationToast').toast({delay: 10000});
     $('#registrationToast').toast('show');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var toastEl = document.getElementById('registrationToast');
+    if (toastEl) {
+        var toast = new bootstrap.Toast(toastEl);
+        toast.show();
+    }
+});
