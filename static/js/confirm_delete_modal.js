742056b31655a66f0d605b6096294bd347193f94
Evita Orrock <evita.orrock@gmail.com>
2024-06-28 12:25:21 +0000

    Add alerts for creating/deleting issues/projects

--- static/js/confirm_delete_modal.js
+++ static/js/confirm_delete_modal.js
@@ -1,31 +1,3 @@
-function showToast(message, type) {
-    const toastContainer = document.createElement('div');
-    toastContainer.className = `toast bg-${type}`;
-    toastContainer.role = 'alert';
-    toastContainer.ariaLive = 'assertive';
-    toastContainer.ariaAtomic = 'true';
-    toastContainer.dataset.delay = '5000';
-
-    toastContainer.innerHTML = `
-        <div class="toast-header">
-            <strong class="me-auto">Notification</strong>
-            <small class="text-muted">just now</small>
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-        </div>
-        <div class="toast-body">
-            ${message}
-        </div>
-    `;
-
-    document.body.appendChild(toastContainer);
-    const toast = new bootstrap.Toast(toastContainer);
-    toast.show();
-
-    toastContainer.addEventListener('hidden.bs.toast', () => {
-        document.body.removeChild(toastContainer);
-    });
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     const confirmDeleteModal = document.getElementById('confirmDeleteModal');
     const deleteModalTitle = document.getElementById('deleteModalTitle');
@@ -38,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
             deleteModalTitle.innerText = 'Are you sure you want to delete this issue?';
             confirmDeleteBtn.onclick = function() {
                 document.getElementById(`deleteIssueForm_${deleteId}`).submit();
-                showToast('Issue deleted successfully', 'success');
+                sessionStorage.setItem('Alert Type', 'Issue Deleted');
             };
             confirmDeleteModal.style.display = 'block';
         });
@@ -50,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
             deleteModalTitle.innerText = 'Are you sure you want to delete this project?';
             confirmDeleteBtn.onclick = function() {
                 document.getElementById(`deleteProjectForm_${deleteId}`).submit();
-                showToast('Project deleted successfully', 'success');
+                sessionStorage.setItem('Alert Type', 'Project Deleted');
             };
             confirmDeleteModal.style.display = 'block';
         });
